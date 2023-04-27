66adcfecfb560b0c72c262c8f1c23c586e0fc79b
yuki <yuki.laurentiu@gmail.com>
2023-04-27 12:58:58 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+   const XylophoneApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
