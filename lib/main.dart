a1c1182a25a269b5a02ff55ea7870edd3c69644b
yuki <yuki.laurentiu@gmail.com>
2023-05-02 12:29:53 -0600

    added audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-   const XylophoneApp({Key? key}) : super(key: key);
+  const XylophoneApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+              child: const Text('Click me'),
+            ),
+          ),
         ),
       ),
     );
